b52512dda0d323acbc0fdff9afbb8cb013f59400
Shashi <sharmashashi940@gmail.com>
2026-04-01 08:27:00 +0545

    add copilot lua for auth

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+	"zbirenbaum/copilot.lua",
+	requires = {
+		"copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+	},
+	config = function()
+		require("copilot").setup({})
+	end,
+}
